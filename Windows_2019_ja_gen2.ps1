56f30fc3610fb02101e98e44ace68ecea1511c62
waisu <waisu70@gmail.com>
2023-07-25 14:06:40 +0900

    Update Windows_2019_ja_gen2.ps1

--- Windows_2019_ja_gen2.ps1
+++ Windows_2019_ja_gen2.ps1
@@ -11,17 +11,5 @@ Add-WindowsPackage -Online -PackagePath $wintemp\$lpfile
 Set-WinSystemLocale -SystemLocale ja-JP
 Set-TimeZone -Id "Tokyo Standard Time"
 
-# RunOnce
-$unattendxml = $workdir + "unattend.xml"
-
-$xml = @"
-<gs:GlobalizationServices xmlns:gs="urn:longhornGlobalizationUnattend">
-<gs:UserList>
-<gs:User UserID="Current" CopySettingsToDefaultUserAcct="true" CopySettingsToSystemAcct="true"/>
-</gs:UserList>
-</gs:GlobalizationServices>
-"@
-$xml | Out-File -FilePath $unattendxml
-
 Restart-Computer -Force
 
