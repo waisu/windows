ba7bc6dccdb0d17f08a32c122dfd5a6f3be7158c
waisu <waisu70@gmail.com>
2023-07-25 14:05:34 +0900

    Update setuplang.ps1

--- setuplang.ps1
+++ setuplang.ps1
@@ -1,3 +1,16 @@
+$workdir = "D:\"
+$unattendxml = $workdir + "unattend.xml"
+
+$xml = @"
+<gs:GlobalizationServices xmlns:gs="urn:longhornGlobalizationUnattend">
+<gs:UserList>
+<gs:User UserID="Current" CopySettingsToDefaultUserAcct="true" CopySettingsToSystemAcct="true"/>
+</gs:UserList>
+</gs:GlobalizationServices>
+"@
+$xml | Out-File -FilePath $unattendxml
+
+
 Set-WinHomeLocation -GeoId 122
 Set-WinUserLanguageList -LanguageList ja-JP -Force
 Set-WinUILanguageOverride -Language ja-JP
@@ -5,9 +18,5 @@ Set-WinCultureFromLanguageListOptOut -OptOut 0
 Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
 Set-ItemProperty "registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\i8042prt\Parameters" -Name "LayerDriver JPN" -Value "kbd106.dll"
 control.exe "intl.cpl,,/f:```"$unattendxml```""
-Set-ItemProperty -Path "$autologonreg" -Name "AutoAdminLogon" -Value "0"
-Set-ItemProperty -Path "$autologonreg" -Name "DefaultUserName" -Value ""
-Set-ItemProperty -Path "$autologonreg" -Name "DefaultPassword" -Value ""
+
 Restart-Computer -Force
-
-
