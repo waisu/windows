0b732541a5016198e8a56a2d468b18a8c480d908
ken <waisu70@gmail.com>
2023-08-08 12:00:55 +0900

    Update Windows_2022_ja_gen2.ps1

--- Windows_2022_ja_gen2.ps1
+++ Windows_2022_ja_gen2.ps1
@@ -1,13 +1,19 @@
 
 #windows japanese localization
 $wintemp = "C:\Windows\Temp"
-$lpurl = "http://kikawadiag.blob.core.windows.net/vhds"
+$lpurl = "https://mkiimage.blob.core.windows.net/vhd" 
 $lpfile ="LpTemp2022.cab"
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
 
-
-Start-BitsTransfer -Source $lpurl/$lpfile -Destination $wintemp
+Invoke-WebRequest "https://mkiimage.blob.core.windows.net/vhd/LpTemp2022.cab" -OutFile $wintemp\$lpfile
 Add-WindowsPackage -Online -PackagePath $wintemp\$lpfile
 
 Set-WinSystemLocale -SystemLocale ja-JP
 Set-TimeZone -Id "Tokyo Standard Time"
+
+Add-WindowsCapability -Online -Name 'Language.Basic~~~ja-JP~0.0.1.0' -Verbose  
+Add-WindowsCapability -Online -Name 'Language.Fonts.Jpan~~~und-JPAN~0.0.1.0' -Verbose 
+Add-WindowsCapability -Online -Name 'Language.OCR~~~ja-JP~0.0.1.0' -Verbose 
+Add-WindowsCapability -Online -Name 'Language.Handwriting~~~ja-JP~0.0.1.0' -Verbose   # Optional 
+Add-WindowsCapability -Online -Name 'Language.Speech~~~ja-JP~0.0.1.0' -Verbose        # Optional 
+Add-WindowsCapability -Online -Name 'Language.TextToSpeech~~~ja-JP~0.0.1.0' -Verbose  # Optional 
